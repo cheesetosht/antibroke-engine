b4a88cacbaaa6c553a1ebd9b988f56f37bd28346
Vraj Shah <thevrajshah@gmail.com>
2020-09-19 18:11:46 +0530

    initial commit after making required changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+import Nav from "./Nav";
+
+export default class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <Nav />
+      </>
+    );
+  }
+}
